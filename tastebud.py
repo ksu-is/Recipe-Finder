71dd455584980d6aedd6c1dc81511d1117ea1c6a
Clayton Merritt <142240294+Clayy52@users.noreply.github.com>
2023-11-21 14:14:41 -0500

    Update tastebud.py
    Create Variables and ingredient input

--- tastebud.py
+++ tastebud.py
@@ -1,1 +1,22 @@
-t
+recipes =[["Recipe Name", "Ingredient 1", "Ingredient 2"],
+          ["Recipe Name 2", "Ingredient 1"]]
+ingredients = []
+
+print("Welcome to TasteBud!")
+print("Please enter the ingredients you have on hand(type exit to stop input)")
+
+ingredientInput = ""
+loopCounter = 1
+while ingredientInput != "exit":
+    ingredientInput = input("Ingredient # "+str(loopCounter)+":")
+    if ingredientInput == "exit":
+        break
+
+    ingredients.append(ingredientInput)
+    loopCounter = loopCounter + 1
+
+print("")
+
+
+print("Ingredients entered:"+str(ingredients))
+

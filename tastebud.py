59e9d215d52fe1808bfb22e95aeadc167def70ce
Clayton Merritt <142240294+Clayy52@users.noreply.github.com>
2023-12-09 16:56:09 -0500

    add shopping lists and partially complete recipes
    added making shopping lists with missing ingredients

--- tastebud.py
+++ tastebud.py
@@ -1,6 +1,9 @@
-recipes =[["Epic Baba Ganoush", "eggplant", "garlic","lemon juice","tahini","olive oil","parsley","salt","cumin","paprika"],
-          ["Rice Krispy Treats", "butter","marshmallow","rice krispies"]]
+recipes =[["Epic Baba Ganoush", "eggplant", "garlic","lemon juice", "tahini", "olive oil","parsley", "salt", "cumin", "paprika"],
+          ["Rice Krispy Treats", "butter", "marshmallow", "rice krispies"],["Baked Chicken","chicken","butter","salt","pepper","garlic powder","paprika"]]
 ingredients = []
+
+
+
 
 print("Welcome to TasteBud!")
 print("Please enter the ingredients you have on hand(type exit to stop input)")
@@ -12,24 +15,52 @@ while ingredientInput != "exit":
     if ingredientInput == "exit":
         break
 
-    ingredients.append(ingredientInput)
+    ingredients.append(ingredientInput.lower())
     loopCounter = loopCounter + 1
 
 print("Ingredients entered:"+str(ingredients))
 
 completeRecipes = []
 
+
+
 numberOfIngredients = 0
 for recipe in recipes:
     for recipeIngredient in recipe:
         if recipeIngredient in ingredients:
             numberOfIngredients = numberOfIngredients + 1
-            print("Ingredient Detected "+recipeIngredient +" Ingredient number"+str(numberOfIngredients))
-    print("length of recipe "+str(len(recipe)-1))
-    print("length of recipe " + str(len(recipe) - 1))
+            #print("Ingredient Detected "+recipeIngredient +" Ingredient number"+str(numberOfIngredients))
+    #print("length of recipe "+str(len(recipe)-1))
+    #print("length of recipe " + str(len(recipe) - 1))
     if numberOfIngredients == len(recipe)-1:
         completeRecipes.append(recipe)
 
+
 print("These are the complete recipes you can make: "+str(completeRecipes))
+
+
+print("These are the recipes you are almost able to make")
+almostCompleteRecipes = []
+missingIngredients = []
+numMissing = 0
+
+for recipe in recipes:
+    numMissing = 0
+    missingIngredients = []
+    for recipeIngredient in recipe:
+        if recipeIngredient not in ingredients:
+            numMissing = numMissing + 1
+            missingIngredients.append(recipeIngredient)
+            #print(recipeIngredient + " was missing!!!")
+    if numMissing >1:
+        print(missingIngredients.pop(0)+" Shopping List:")
+
+        for missingIng in missingIngredients:
+            print("- " + missingIng)
+
+print("Thank you for using tastebud!!!!!")
+
+
+
 
 
